12a9a572342df3b2a296e88700659573390c3096
CYTech Student <CYTech Student@192.168.1.136>
2024-03-26 14:32:41 +0100

    Modification classe Producteur : redéfinition toString + methodes ajouter et supprimer film mises à jour

--- Projet_Java/src/metier/Producteur.java
+++ Projet_Java/src/metier/Producteur.java
@@ -1,6 +1,5 @@
 package metier;
 
-
 import java.util.ArrayList;
 
 public class Producteur {
@@ -14,10 +13,6 @@ public class Producteur {
         this.prenom = prenom;
         this.films = new ArrayList<Film>();
     }
-
-    // Methods from the class diagram
-    public void ajouter_film(Film film) {}
-    public void supprimer_film(Film film) {}
 
     // Getters and setters
     public String getNom() {
@@ -42,5 +37,31 @@ public class Producteur {
 
     public void setFilms(ArrayList<Film> films) {
         this.films = new ArrayList<Film>(films);
+    }
+    
+    // Methodes
+    public void ajouter_film(Film film) {
+        this.films.add(film);
+    }
+
+    public void supprimer_film(Film film) {
+        this.films.remove(film);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producteur: ").append(this.prenom).append(" ").append(this.nom).append("\n");
+        sb.append("Films produits:\n");
+        
+        if (films.isEmpty()) {
+            sb.append("Aucun film produit.\n");
+        } else {
+            for (Film film : films) {
+                sb.append(" - ").append(film.getTitre()).append(" (").append(film.getAnnee_prod()).append(")\n");
+            }
+        }
+
+        return sb.toString();
     }
 }
